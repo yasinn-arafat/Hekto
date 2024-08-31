ca5c6fcee54eed8bccd4faddd4773a91031e648d
yasinn-arafat <yasinarafat4870@gmail.com>
2024-08-31 20:25:02 +0600

    FeaturedTitle  added

---
+++ src/Component/CommonComponent/Title/Title.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div>
+      <h2 className="font-JosefinFont text-[42px] text-[#151875] font-bold text-center">
+        Featured Products
+      </h2>
+    </div>
+  );
+};
+
+export default Title;
--- src/Component/HomeComponent/FeaturedProducts/FeaturedProducts.jsx
+++ src/Component/HomeComponent/FeaturedProducts/FeaturedProducts.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Title from "../../CommonComponent/Title/Title";
 
 const FeaturedProducts = () => {
   return (
     <>
       <div className="p-6">
         <div className="container">
+          <Title />
           <div className="flex items-center gap-x-7">
             <div></div>
             <div></div>
