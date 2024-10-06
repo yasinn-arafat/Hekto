d8a056d6ed0b2a46c989d641b562f0f511979189
yasinn-arafat <yasinarafat4870@gmail.com>
2024-10-06 23:17:10 +0600

    Modified

--- src/Component/HomeComponent/TrendingProducts/TrendingProducts.jsx
+++ src/Component/HomeComponent/TrendingProducts/TrendingProducts.jsx
@@ -1,14 +1,45 @@
 import React from "react";
 import CommonTitle from "../../CommonComponent/CommonTitle/CommonTitle";
+import OfferCard from "../../CommonComponent/OfferCard/OfferCard";
+import TrendingProductImg from "../../../assets/TrendingProduct3.png";
 
 const TrendingProducts = () => {
   return (
     <>
-      <div className="py-28">
+      <div className="py-28 bg-[#FFFFFF]">
         <div className="container">
           <div>
+            <CommonTitle Title={"Trending Products"} />
             <div>
-              <CommonTitle Title={"Trending Products"} />
+              <OfferCard
+                className={
+                  "bg-white shadow-black w-[270px] h-[350px] flex flex-col items-center"
+                }
+              >
+                <div>
+                  <picture>
+                    <img
+                      src={TrendingProductImg}
+                      alt={TrendingProductImg}
+                      className="pt-3"
+                    />
+                  </picture>
+                </div>
+
+                <div className="pt-[15px] flex flex-col items-center">
+                  <h6 className="text-[#151875] font-LatoFont font-bold text-base">
+                    Cantilever chair
+                  </h6>
+                  <div className="flex items-center gap-x-3">
+                    <h6 className="text-[#151875] font-JosefinFont font-normal text-sm">
+                      $26.00
+                    </h6>
+                    <p className="text-[#151875] font-JosefinFont font-normal text-xs opacity-30">
+                      $42.00
+                    </p>
+                  </div>
+                </div>
+              </OfferCard>
             </div>
           </div>
         </div>
