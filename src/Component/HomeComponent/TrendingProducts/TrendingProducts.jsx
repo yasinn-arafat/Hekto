9f61554ea8ad3eb449124378c606317cd9603282
yasinn-arafat <yasinarafat4870@gmail.com>
2024-10-09 23:16:49 +0600

    Trending Product Part Complete

--- src/Component/HomeComponent/TrendingProducts/TrendingProducts.jsx
+++ src/Component/HomeComponent/TrendingProducts/TrendingProducts.jsx
@@ -10,10 +10,10 @@ const TrendingProducts = () => {
         <div className="container">
           <div>
             <CommonTitle Title={"Trending Products"} />
-            <div>
+            <div className="flex items-center gap-x-12 justify-center pt-10 cursor-pointer ">
               <OfferCard
                 className={
-                  "bg-white shadow-black w-[270px] h-[350px] flex flex-col items-center"
+                  "bg-white shadow-2xl w-[270px] h-[350px] flex flex-col items-center hover:scale-110 duration-150"
                 }
               >
                 <div>
@@ -34,7 +34,97 @@ const TrendingProducts = () => {
                     <h6 className="text-[#151875] font-JosefinFont font-normal text-sm">
                       $26.00
                     </h6>
-                    <p className="text-[#151875] font-JosefinFont font-normal text-xs opacity-30">
+                    <p className="text-[#151875] font-JosefinFont line-through font-normal text-xs opacity-30">
+                      $42.00
+                    </p>
+                  </div>
+                </div>
+              </OfferCard>
+
+              <OfferCard
+                className={
+                  "bg-white shadow-2xl w-[270px] hover:scale-110 duration-150 h-[350px] flex flex-col items-center"
+                }
+              >
+                <div>
+                  <picture>
+                    <img
+                      src={TrendingProductImg}
+                      alt={TrendingProductImg}
+                      className="pt-3"
+                    />
+                  </picture>
+                </div>
+
+                <div className="pt-[15px] flex flex-col items-center">
+                  <h6 className="text-[#151875] font-LatoFont font-bold text-base">
+                    Cantilever chair
+                  </h6>
+                  <div className="flex items-center gap-x-3">
+                    <h6 className="text-[#151875] font-JosefinFont font-normal text-sm">
+                      $26.00
+                    </h6>
+                    <p className="text-[#151875] font-JosefinFont line-through font-normal text-xs opacity-30">
+                      $42.00
+                    </p>
+                  </div>
+                </div>
+              </OfferCard>
+
+              <OfferCard
+                className={
+                  "bg-white shadow-2xl w-[270px] h-[350px] flex flex-col items-center hover:scale-110 duration-150"
+                }
+              >
+                <div>
+                  <picture>
+                    <img
+                      src={TrendingProductImg}
+                      alt={TrendingProductImg}
+                      className="pt-3"
+                    />
+                  </picture>
+                </div>
+
+                <div className="pt-[15px] flex flex-col items-center">
+                  <h6 className="text-[#151875] font-LatoFont font-bold text-base">
+                    Cantilever chair
+                  </h6>
+                  <div className="flex items-center gap-x-3">
+                    <h6 className="text-[#151875] font-JosefinFont font-normal text-sm">
+                      $26.00
+                    </h6>
+                    <p className="text-[#151875] font-JosefinFont line-through font-normal text-xs opacity-30">
+                      $42.00
+                    </p>
+                  </div>
+                </div>
+              </OfferCard>
+
+              <OfferCard
+                className={
+                  "bg-white shadow-2xl w-[270px] h-[350px] flex flex-col items-center hover:scale-110 duration-150"
+                }
+              >
+                <div>
+                  <picture>
+                    <img
+                      src={TrendingProductImg}
+                      alt={TrendingProductImg}
+                      className="pt-3"
+                    />
+                  </picture>
+                </div>
+
+                <div className="pt-[15px] flex flex-col items-center">
+                  <h6 className="text-[#151875] font-LatoFont font-bold text-base">
+                    Cantilever chair
+                  </h6>
+                  <div className="flex items-center gap-x-3">
+                    <h6 className="text-[#151875] font-JosefinFont font-normal text-sm">
+                      $26.00
+                    </h6>
+                    <p className="text-[#151875] font-JosefinFont line-through font-normal text-xs opacity-30">
                       $42.00
                     </p>
                   </div>
