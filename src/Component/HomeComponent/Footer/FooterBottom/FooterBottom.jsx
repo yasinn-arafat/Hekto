3b75b60225c1ad5a14fcae107dbd62abb3d81dc0
yasinn-arafat <yasinarafat4870@gmail.com>
2024-10-31 23:09:45 +0600

    Modified

--- src/Component/HomeComponent/Footer/FooterBottom/FooterBottom.jsx
+++ src/Component/HomeComponent/Footer/FooterBottom/FooterBottom.jsx
@@ -17,7 +17,7 @@ const FooterBottom = () => {
                   <FaFacebookF />
                 </a>
               </div>
-              <div className="flex items-center justify-center w-6 h-6 rounded-full bg-[#151875]">
+              <div className="flex items-center  justify-center w-6 h-6 rounded-full bg-[#151875]">
                 <a href="#" className="text-white">
                   <AiFillInstagram />
                 </a>
